d247e08fcc86c6f6cd7607a440339a9020ced0aa
Kai_EV <kasicer1@gmail.com>
2022-12-01 22:06:47 +0100

    Add activity Consult

---
+++ app/src/main/java/com/example/evaluaciont1_kaiscerv/ConsultResultActivity.java
@@ -0,0 +1,14 @@
+package com.example.evaluaciont1_kaiscerv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConsultResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consult_result);
+    }
+}
